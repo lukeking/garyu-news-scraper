5970b1317fbbae31e1705ebe619f5b4730a1621a
Luke <lukeking0325@gmail.com>
2026-04-30 22:49:45 +0800

    debug: add debug log

--- docs/functions/api/[[path]].js
+++ docs/functions/api/[[path]].js
@@ -4,5 +4,6 @@ export async function onRequest(context) {
   // Optional: Remove /api if your worker expects routes to start at root /
   // url.pathname = url.pathname.replace(/^\/api/, '');
 
+  console.log(`Received request for ${url.pathname}`, JSON.stringify(context.env, null, 2));
   return await context.env.API.fetch(new Request(url, context.request));
 }
