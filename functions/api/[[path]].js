01e26a9cbc376f12d751e95804c9fb553b36b92f
Luke <lukeking0325@gmail.com>
2026-04-30 23:01:42 +0800

    chore: remove debug info

--- functions/api/[[path]].js
+++ functions/api/[[path]].js
@@ -4,6 +4,5 @@ export async function onRequest(context) {
   // Optional: Remove /api if your worker expects routes to start at root /
   // url.pathname = url.pathname.replace(/^\/api/, '');
 
-  console.log(`Received request for ${url.pathname}`, JSON.stringify(context.env, null, 2));
   return await context.env.API.fetch(new Request(url, context.request));
 }
